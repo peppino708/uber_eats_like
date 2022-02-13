02b5651578081be87382f112af8e592e9760a192
github peppino708 <708.baseball.ster@gmail.com>
2022-02-13 17:29:29 +0900

    Add memo order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ApplicationRecord
 
   validates :total_price, numericality: { greater_than: 0 }
 
+
+  # line_food.update_attributes!とself.save!の２つの処理に対してトランザクションを張っていることがわかります。どちらか片方でも失敗した場合にこsave_with_update_line_foods!は失敗となり、ロールバックしてくれます。
   def save_with_update_line_foods!(line_foods)
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
