29f0c66668742b896041d190340fbd889af95426
github peppino708 <708.baseball.ster@gmail.com>
2022-02-16 22:07:55 +0900

    Create constant.js,style_constant.js

---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+//複数のコンポーネント、関数、モジュールから参照される値は別ファイルに定義しておく
+export const REQUEST_STATE = {
+  INITIAL: "INITIAL",
+  LOADING: "LOADING",
+  OK: "OK",
+};
+
+export const HTTP_STATUS_CODE = {
+  NOT_ACCEPTABLE: 406,
+};
